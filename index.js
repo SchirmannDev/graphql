4f212b727af4b91d9e968c6d596d49fd74c07598
Cintia Schirmann <cschirmann.dev@gmail.com>
2022-05-24 22:58:31 -0300

    input e mutation

--- index.js
+++ index.js
@@ -1,70 +1,14 @@
-const { gql, ApolloServer } = require("apollo-server");
+const { ApolloServer } = require("apollo-server");
 
-const db = [
-  {
-    id: 1,
-    email: "cintia@email.com",
-    name: "Cintia",
-    phone: " 12 334 566",
-    profile: 1,
-  },
-  {
-    id: 2,
-    email: "antonella@email.com",
-    name: "Antonella",
-    phone: "123 345 996",
-    profile: 2,
-  },
-];
-
-const profiles = [
-  {
-    id: 1,
-    description: "ADMIN",
-  },
-  {
-    id: 2,
-    description: "USER",
-  },
-];
-
-const typeDefs = gql`
-  type User {
-    phone: String
-    name: String
-    email: String
-    id: ID
-    profile: Profile
-  }
-  type Profile {
-    id: Int
-    description: String
-  }
-  type Query {
-    user(id: Int): User
-    profiles: [Profile]
-  }
-`;
-
-const resolvers = {
-  User: {
-    profile(user) {
-      return profiles.find((profile) => profile.id === user.profile);
-    },
-  },
-  Query: {
-    user(_, args) {
-      return db.find((db) => db.id === args.id);
-    },
-    profiles() {
-      return profiles;
-    },
-  },
-};
+const graphql = require("./src/graphql");
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  ...graphql,
+  formatError: (error) => {
+    if (error.message.startsWith("Usuario Existente:")) {
+      return new Error(error.message);
+    }
+  },
 });
 
-server.listen();
+server.listen().then(({ url }) => console.log("Server running on " + url));
---
+++ src/graphql/modules/Profile/resolvers.js
@@ -0,0 +1,8 @@
+const db = require("../../../db");
+module.exports = {
+  Query: {
+    profiles() {
+      return db.profiles;
+    },
+  },
+};
